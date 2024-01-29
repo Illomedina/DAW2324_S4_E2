20216dd29176eded394fad7d3bcabe9faa8abebf
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-01-29 20:22:56 +0100

    products page 1st version

--- react/src/router/Router.jsx
+++ react/src/router/Router.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import Login from '../pages/auth/Login'
-import PageNotFound from '../pages/404/PageNotFound'
+import Login from '../pages/auth/Login';
+import PageNotFound from '../pages/404/PageNotFound';
+import ProductsPage from '../pages/products/ProductsPage';
 
 export const Router = () => {
 
-return (
+  return (
     <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/*" element={<PageNotFound />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/*" element={<PageNotFound />} />
+      <Route path="/products" element={<ProductsPage />} />
     </Routes>
   );
 };
 
 export default Router;
 
- 
+
