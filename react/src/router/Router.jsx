bd8bc94fab5e19b5e78c503ff9d78bb56a92586b
Riba00 <jordi.riba.ferre@gmail.com>
2024-01-31 17:11:43 +0100

    Sidebar

--- react/src/router/Router.jsx
+++ react/src/router/Router.jsx
@@ -3,12 +3,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import Login from '../pages/auth/Login'
 import PageNotFound from '../pages/404/PageNotFound'
+import DashboardPage from '../pages/dashboard/DashboardPage'
 
 export const Router = () => {
 
 return (
     <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/*" element={<PageNotFound />} />
     </Routes>
   );
