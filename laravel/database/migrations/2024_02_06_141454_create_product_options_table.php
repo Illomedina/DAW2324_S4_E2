798266c963b8e8bdb31b6d55f72765bb7e8bdac6
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-02-06 15:17:51 +0100

    add tables product options & product option values

---
+++ laravel/database/migrations/2024_02_06_141454_create_product_options_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_options', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('idProduct')->references('id')->on('products')->onDelete('cascade');
+            $table->string('option_id_picanova');
+            $table->string('image')->nullable();
+            $table->string('description')->nullable();
+            $table->boolean('is_required');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_options');
+    }
+};
