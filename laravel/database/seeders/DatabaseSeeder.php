736d8a7c594bc799f6b8fe0e183b86c2a1780383
Miquel <miquelduran@iesmontsia.org>
2024-03-20 20:10:22 +0100

    arreglat fallo del seeder de orders

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -31,6 +31,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             BenefitsTableSeeder::class,
+            OrderTableSeeder::class,
+            OrderDetailSeeder::class,
         ]);
         
     }
