6c0baed3a6b66ad040c0c82a327ad6cbfa65cbec
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-03-26 20:38:45 +0100

    :hammer: fixing namespaces

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\setting\SettingController;
+use App\Http\Controllers\SettingController;
 use App\Http\Controllers\BenefitsController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderDetailsController;
